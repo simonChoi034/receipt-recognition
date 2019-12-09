0623676576261d23b935290521e4b592280c766e
simonChoi034 <simonchoi034@gmail.com>
2019-12-09 16:48:50 +0800

    Filter faulty image

---
+++ coco_text/clean_image.py
@@ -0,0 +1,15 @@
+from skimage import io
+import os
+
+error_image = []
+
+dir = input("image dir: ")
+
+for img in os.listdir(dir):
+    a = io.imread(dir + img)
+    if len(a.shape) != 3 or a.shape[2] != 3:
+        print(img)
+        error_image.append(img)
+
+print("Image that is not in RGB channel:")
+print(error_image)
